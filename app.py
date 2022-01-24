3b110f558e2d4e411b0ae05f207a95955aeb37c2
dirtywizzard <marcin.jakimiuk@gmail.com>
2022-01-24 20:02:51 +0100

    fixed errors

--- app.py
+++ app.py
@@ -1,49 +1,54 @@
 from flask import Flask, request, render_template
-from datetime import datetime, date
+from datetime import date
 
-from mj_logic import days_from_now_logic, today_str_func, week_dates_logic
+from mj_logic import days_from_now_logic, week_dates_logic
 
 app = Flask(__name__)
+
+
+today = str(date.today())
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/days_from_now", methods=['GET','POST'])
+@app.route("/days_from_now", methods=['GET', 'POST'])
 def days_from_now():
+    global today
     name = ""
-    today_str_txt = today_str_func()
-    today_str = today_str_func()
+    today_str_txt = today
+    today_str = today
     if request.method == "POST" and "input_date" in request.form:
         name = request.form.get("input_date")
         name = days_from_now_logic(name)
         today_str = request.form.get("input_date")
-    
     return render_template("days_from_now.html",
-                            name = name,
-                            today_str = today_str,
-                            today_str_txt = today_str_txt)
+                           name=name,
+                           today_str=today_str,
+                           today_str_txt=today_str_txt)
 
 
-@app.route("/week_dates", methods=['GET','POST'])
+@app.route("/week_dates", methods=['GET', 'POST'])
 def week_dates():
-    today_str = today_str_func()
+    global today
+    today_str = today
     date_1 = ""
     date_Year = ""
     date_Week = ""
     date_2 = ""
-    
-    if request.method == "POST" and "input_Year" in request.form and "input_Week" in request.form:
+    if (request.method == "POST" and "input_Year" in request.form
+       and "input_Week" in request.form):
         date_Year = request.form.get("input_Year")
         date_Week = request.form.get("input_Week")
-        date_1 = week_dates_logic(date_Year,date_Week)[0]
-        date_2 = week_dates_logic(date_Year,date_Week)[1]
+        date_1 = week_dates_logic(date_Year, date_Week)[0]
+        date_2 = week_dates_logic(date_Year, date_Week)[1]
 
     return render_template("week_dates.html",
-                            today_str = today_str,
-                            date_1 = date_1,
-                            date_2 = date_2)
+                           today_str=today_str,
+                           date_1=date_1,
+                           date_2=date_2)
 
 
 @app.route("/holiday_planner")
--- mj_logic.py
+++ mj_logic.py
@@ -1,22 +1,24 @@
 from datetime import datetime, date
+import typing
 
-def days_from_now_logic(date_str: str) -> int:
+
+def days_from_now_logic(date_str: str) -> str:
     try:
         dt = datetime.strptime(date_str, "%Y-%m-%d")
         today = datetime.combine(date.today(), datetime.min.time())
-        return (dt-today).days
+        return str((dt-today).days)
     except ValueError:
         return 'error'
-    
 
-def today_str_func():
-    return datetime.today().date().strftime("%Y-%m-%d")
 
-def week_dates_logic(year: str,week: str) -> str:
+def week_dates_logic(year: str, week: str) -> typing.Tuple[str, str]:
+    const_1 = "%Y-W%W-%w"
+    const_2 = "%d/%m/%Y"
     try:
-        d = f"{year}-W{week}"
-        r = datetime.strptime(d + '-1', "%Y-W%W-%w")
-        q = datetime.strptime(d + '-0', "%Y-W%W-%w")
-        return r.date().strftime("%d/%m/%Y"),q.date().strftime("%d/%m/%Y")
+        week_str = f"{year}-W{week}"
+        range_1 = datetime.strptime(week_str + '-1', const_1)
+        range_2 = datetime.strptime(week_str + '-0', const_1)
+        return (range_1.date().strftime(const_2),
+                range_2.date().strftime(const_2))
     except ValueError:
-        return ('error','error')
+        return ('error', 'error')
