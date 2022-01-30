bf2832b6a3c6dafe3e66f31deee0d9416c1d5cb2
dirtywizzard <marcin.jakimiuk@gmail.com>
2022-01-30 13:33:13 +0100

    added calendar

--- app.py
+++ app.py
@@ -1,13 +1,109 @@
 from flask import Flask, request, render_template
-from datetime import date
+from datetime import date, datetime
 
-from mj_logic import days_from_now_logic, week_dates_logic
+from mj_logic import days_from_now_logic, week_dates_logic, calendar_holiday
+
+from flask import Markup
+import calendar
+import holidays 
 
 app = Flask(__name__)
 
 
 today = str(date.today())
+TODAY_int = date.today().year
+YEARS = [str(i) for i in range(TODAY_int,TODAY_int + 6)]
 
+COUNTRIES={"Angola":holidays.AO(),
+    "Argentina":holidays.AR(),
+    "Aruba":holidays.AW(),
+    "Australia":holidays.AU(),
+    "Austria":holidays.AT(),
+    "Azerbaijan":holidays.AZ(),
+    "Bangladesh":holidays.BD(),
+    "Belarus":holidays.BY(),
+    "Belgium":holidays.BE(),
+    "Botswana":holidays.BW(),
+    "Brazil":holidays.BR(),
+    "Bulgaria":holidays.BG(),
+    "Burundi":holidays.BI(),
+    "Canada":holidays.CA(),
+    "Chile":holidays.CL(),
+    "China":holidays.CN(),
+    "Colombia":holidays.CO(),
+    "Croatia":holidays.HR(),
+    "Curacao":holidays.CW(),
+    "Czechia":holidays.CZ(),
+    "Denmark":holidays.DK(),
+    "Djibouti":holidays.DJ(),
+    "DominicanRepublic":holidays.DO(),
+    "Egypt":holidays.EG(),
+    "Estonia":holidays.EE(),
+    "Ethiopia":holidays.ET(),
+    "EuropeanCentralBank":holidays.ECB(),
+    "Finland":holidays.FI(),
+    "France":holidays.FR(),
+    "Georgia":holidays.GE(),
+    "Germany":holidays.DE(),
+    "Greece":holidays.GR(),
+    "Honduras":holidays.HN(),
+    "HongKong":holidays.HK(),
+    "Hungary":holidays.HU(),
+    "Iceland":holidays.IS(),
+    "India":holidays.IN(),
+    "Ireland":holidays.IE(),
+    "Israel":holidays.IL(),
+    "Italy":holidays.IT(),
+    "Jamaica":holidays.JM(),
+    "Japan":holidays.JP(),
+    "Kazakhstan":holidays.KZ(),
+    "Kenya":holidays.KE(),
+    "Korea":holidays.KR(),
+    "Latvia":holidays.LV(),
+    "Lesotho":holidays.LS(),
+    "Lithuania":holidays.LT(),
+    "Luxembourg":holidays.LU(),
+    "Malaysia":holidays.MY(),
+    "Malawi":holidays.MW(),
+    "Mexico":holidays.MX(),
+    "Morocco":holidays.MA(),
+    "Mozambique":holidays.MZ(),
+    "Netherlands":holidays.NL(),
+    "Namibia":holidays.NA(),
+    "NewZealand":holidays.NZ(),
+    "Nicaragua":holidays.NI(),
+    "Nigeria":holidays.NG(),
+    "NorthMacedonia":holidays.MK(),
+    "Norway":holidays.NO(),
+    "Paraguay":holidays.PY(),
+    "Peru":holidays.PE(),
+    "Poland":holidays.PL(),
+    "Portugal":holidays.PT(),
+    "PortugalExt":holidays.PTE(),
+    "Romania":holidays.RO(),
+    "Russia":holidays.RU(),
+    "SaudiArabia":holidays.SA(),
+    "Serbia":holidays.RS(),
+    "Singapore":holidays.SG(),
+    "Slovakia":holidays.SK(),
+    "Slovenia":holidays.SI(),
+    "SouthAfrica":holidays.ZA(),
+    "Spain":holidays.ES(),
+    "Swaziland":holidays.SZ(),
+    "Sweden":holidays.SE(),
+    "Switzerland":holidays.CH(),
+    "Taiwan":holidays.TW(),
+    "Turkey":holidays.TR(),
+    "Tunisia":holidays.TN(),
+    "Ukraine":holidays.UA(),
+    "UnitedArabEmirates":holidays.AE(),
+    "UnitedKingdom":holidays.UK(),
+    "UnitedStates":holidays.US(),
+    "Venezuela":holidays.VE(),
+    "Vietnam":holidays.VN(),
+    "Zambia":holidays.ZM(),
+    "Zimbabwe":holidays.ZW(),
+}
 
 @app.route("/")
 def index():
@@ -51,6 +147,30 @@ def week_dates():
                            date_2=date_2)
 
 
-@app.route("/holiday_planner")
+@app.route("/holiday_planner", methods=['GET', 'POST'])
 def holiday_planner():
-    return render_template("holiday_planner.html")
+    z_cal=''
+    CHOOSEN_COUNTRY=''
+    CHOOSEN_YEAR=''
+    
+    if (request.method == "POST" and request.form.get("input_vacation")
+        and request.form.get("country") in COUNTRIES
+        and request.form.get("year") in YEARS):
+       
+           z_cal = calendar_holiday(int(request.form.get("year")),country=COUNTRIES[request.form.get("country")])
+           CHOOSEN_COUNTRY=request.form.get("country")
+           CHOOSEN_YEAR=request.form.get("year")
+    return render_template("holiday_planner.html",
+                           value1 = z_cal,
+                           countries=COUNTRIES,
+                           years=YEARS,
+                           choosen_country=CHOOSEN_COUNTRY,
+                           choosen_year=CHOOSEN_YEAR)
+
+
+@app.route("/what_date", methods=['GET', 'POST'])
+def what_date():
+    return render_template("what_date.html")
+
+
+
--- mj_logic.py
+++ mj_logic.py
@@ -1,5 +1,7 @@
 from datetime import datetime, date
 import typing
+import holidays
+import calendar 
 
 
 def days_from_now_logic(date_str: str) -> str:
@@ -22,3 +24,20 @@ def week_dates_logic(year: str, week: str) -> typing.Tuple[str, str]:
                 range_2.date().strftime(const_2))
     except ValueError:
         return ('error', 'error')
+
+
+def calendar_holiday(year,country=holidays.US()):
+  cal_generator=[]
+  cal_lst=[]
+  for i in range(1,13):
+    cal_generator.append(calendar.Calendar(firstweekday=0).itermonthdates(year, i))
+  for i in cal_generator:
+    for y in i:
+      if y in country or y.weekday()==6 or y.weekday()==5:
+        cal_lst.append((y,'holiday',y.isocalendar()[1],y.month))
+      else:
+        cal_lst.append((y,'',y.isocalendar()[1],y.month))
+  return [cal_lst[q:x] for q,x in zip(range(0,442,7),range(7,442,7))]
+
+
+
